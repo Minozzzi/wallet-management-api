fc39c74067112f6e554ecf4d92c236d1559fa9f4
Guilherme Minozzi <minozzi60@gmail.com>
2022-09-25 13:34:17 -0300

    feat(category): add filters and pagination with sort to find categories

---
+++ src/main/java/com/walletmanagement/shared/specification/BaseSpecification.java
@@ -0,0 +1,27 @@
+package com.walletmanagement.shared.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public abstract class BaseSpecification<T, U> {
+
+  private static final String WILDCARD = "%";
+
+  public abstract Specification<T> getFilter(U request);
+
+  public Specification<T> entityAttributeContains(String attribute, String value) {
+    return (root, query, cb) -> {
+      if (value == null) {
+        return null;
+      }
+
+      return cb.like(
+          cb.lower(root.get(attribute)),
+          containsLowerCase(value));
+    };
+  }
+
+  private String containsLowerCase(String searchField) {
+    return WILDCARD + searchField.toLowerCase() + WILDCARD;
+  }
+
+}
