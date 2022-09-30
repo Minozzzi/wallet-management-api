655e667ba3474a2bb2dfa2f230e14e94836f6c1d
Guilherme Minozzi <minozzi60@gmail.com>
2022-09-30 17:27:39 -0300

    refactor(category/listAll): use generics on listAll service

---
+++ src/main/java/com/walletmanagement/category/services/listAll/IListAllCategoryService.java
@@ -0,0 +1,7 @@
+package com.walletmanagement.category.services.listAll;
+
+import com.walletmanagement.bases.services.listAll.IBaseListAllService;
+import com.walletmanagement.category.dto.ListAllCategoryResponseDto;
+
+public interface IListAllCategoryService extends IBaseListAllService<ListAllCategoryResponseDto> {
+}
