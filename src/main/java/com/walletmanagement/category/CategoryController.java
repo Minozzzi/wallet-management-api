49c68f448fb2b65e890bdbcf48c0cc5e5790f5e1
Guilherme Minozzi <minozzi60@gmail.com>
2022-10-01 14:32:48 -0300

    refactor(category): use base controller

--- src/main/java/com/walletmanagement/category/CategoryController.java
+++ src/main/java/com/walletmanagement/category/CategoryController.java
@@ -1,69 +1,65 @@
 package com.walletmanagement.category;
 
-import java.util.List;
-import java.util.UUID;
-
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.walletmanagement.bases.controllers.BaseController;
+import com.walletmanagement.bases.services.create.IBaseCreateService;
+import com.walletmanagement.bases.services.delete.IBaseDeleteService;
+import com.walletmanagement.bases.services.list.IBaseListService;
+import com.walletmanagement.bases.services.listAll.IBaseListAllService;
+import com.walletmanagement.bases.services.update.IBaseUpdateService;
 import com.walletmanagement.category.dto.CreateCategoryResponseDto;
 import com.walletmanagement.category.dto.CreateUpdateCategoryDto;
 import com.walletmanagement.category.dto.ListAllCategoryResponseDto;
 import com.walletmanagement.category.dto.ListCategoryDto;
 import com.walletmanagement.category.dto.ListCategoryResponseDto;
-import com.walletmanagement.category.services.CreateCategoryService;
-import com.walletmanagement.category.services.DeleteCategoryService;
-import com.walletmanagement.category.services.ListAllCategoryService;
-import com.walletmanagement.category.services.ListCategoryService;
-import com.walletmanagement.category.services.UpdateCategoryService;
-import com.walletmanagement.shared.dto.PaginationDto;
-import com.walletmanagement.shared.dto.PaginationResponseDto;
 
 @RestController
 @RequestMapping("category")
-public record CategoryController(
-    CreateCategoryService createUpdateCategoryService,
-    UpdateCategoryService updateCategoryService,
-    ListCategoryService listCategoryService,
-    ListAllCategoryService listAllCategoryService,
-    DeleteCategoryService deleteCategoryService) {
+public class CategoryController extends
+    BaseController<CreateUpdateCategoryDto, CreateCategoryResponseDto, ListCategoryDto, ListCategoryResponseDto, ListAllCategoryResponseDto> {
 
-  @PostMapping
-  public ResponseEntity<CreateCategoryResponseDto> create(@Valid @RequestBody CreateUpdateCategoryDto dto) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(createUpdateCategoryService.execute(dto));
+  private final IBaseCreateService<CreateUpdateCategoryDto, CreateCategoryResponseDto> createService;
+  private final IBaseUpdateService<CreateUpdateCategoryDto> updateService;
+  private final IBaseListService<ListCategoryDto, ListCategoryResponseDto> listService;
+  private final IBaseListAllService<ListAllCategoryResponseDto> listAllService;
+  private final IBaseDeleteService deleteService;
+
+  public CategoryController(IBaseCreateService<CreateUpdateCategoryDto, CreateCategoryResponseDto> createService,
+      IBaseUpdateService<CreateUpdateCategoryDto> updateService,
+      IBaseListService<ListCategoryDto, ListCategoryResponseDto> listService,
+      IBaseListAllService<ListAllCategoryResponseDto> listAllService, IBaseDeleteService deleteService) {
+    this.createService = createService;
+    this.updateService = updateService;
+    this.listService = listService;
+    this.listAllService = listAllService;
+    this.deleteService = deleteService;
   }
 
-  @PatchMapping("{id}")
-  public ResponseEntity<Void> update(@PathVariable UUID id, @Valid @RequestBody CreateUpdateCategoryDto dto) {
-    updateCategoryService.execute(id, dto);
-    return ResponseEntity.noContent().build();
+  @Override
+  protected IBaseCreateService<CreateUpdateCategoryDto, CreateCategoryResponseDto> getCreateService() {
+    return createService;
   }
 
-  @GetMapping
-  public ResponseEntity<PaginationResponseDto<ListCategoryResponseDto>> list(PaginationDto pagination,
-      ListCategoryDto filters) {
-    return ResponseEntity.ok(listCategoryService.execute(pagination, filters));
+  @Override
+  protected IBaseUpdateService<CreateUpdateCategoryDto> getUpdateService() {
+    return updateService;
   }
 
-  @GetMapping("all")
-  public ResponseEntity<List<ListAllCategoryResponseDto>> listAll() {
-    return ResponseEntity.ok(listAllCategoryService.execute());
+  @Override
+  protected IBaseListService<ListCategoryDto, ListCategoryResponseDto> getListService() {
+    return listService;
   }
 
-  @DeleteMapping("{id}")
-  public ResponseEntity<Void> delete(@PathVariable UUID id) {
-    deleteCategoryService.execute(id);
-    return ResponseEntity.noContent().build();
+  @Override
+  protected IBaseListAllService<ListAllCategoryResponseDto> getListAllService() {
+    return listAllService;
+  }
+
+  @Override
+  protected IBaseDeleteService getDeleteService() {
+    return deleteService;
   }
 
 }
