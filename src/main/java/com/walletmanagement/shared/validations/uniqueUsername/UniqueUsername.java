16528f170ba4784c9e13d5f23eacf24e6d0964ee
Guilherme Minozzi <minozzi60@gmail.com>
2022-10-05 21:14:37 -0300

    feat: add unique username validation (#8)

---
+++ src/main/java/com/walletmanagement/shared/validations/uniqueUsername/UniqueUsername.java
@@ -0,0 +1,20 @@
+package com.walletmanagement.shared.validations.uniqueUsername;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = UniqueUsernameValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueUsername {
+  String message() default "Username already exists";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
