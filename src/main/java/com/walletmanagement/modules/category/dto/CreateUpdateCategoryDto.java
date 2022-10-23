2f806ff8e25abc0ed8946036b18d6d5b2d27bf78
Guilherme Minozzi <minozzi60@gmail.com>
2022-10-23 12:54:17 -0300

    test: add category test

--- src/main/java/com/walletmanagement/modules/category/dto/CreateUpdateCategoryDto.java
+++ src/main/java/com/walletmanagement/modules/category/dto/CreateUpdateCategoryDto.java
@@ -3,6 +3,8 @@ package com.walletmanagement.modules.category.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,6 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreateUpdateCategoryDto {
 
   @NotNull
