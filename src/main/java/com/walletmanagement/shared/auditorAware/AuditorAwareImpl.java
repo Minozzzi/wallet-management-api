2534758ddc104e3ea819b8834810973b9b20a27b
Guilherme Minozzi <minozzi60@gmail.com>
2022-10-29 17:00:11 -0300

    fix: fixes the user auditor structure

--- src/main/java/com/walletmanagement/shared/auditorAware/AuditorAwareImpl.java
+++ src/main/java/com/walletmanagement/shared/auditorAware/AuditorAwareImpl.java
@@ -6,12 +6,26 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.walletmanagement.entities.UserEntity;
+import com.walletmanagement.modules.user.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class AuditorAwareImpl implements AuditorAware<UserEntity> {
+
+  private static final String ANONYMOUS_USER = "anonymousUser";
+
+  private final UserRepository userRepository;
 
   @Override
   public Optional<UserEntity> getCurrentAuditor() {
-    UserEntity user = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    if (username.equals(ANONYMOUS_USER) || username.isEmpty()) {
+      return Optional.empty();
+    }
+
+    UserEntity user = userRepository.findByUsername(username);
     return Optional.of(user);
   }
 
--- src/main/java/com/walletmanagement/shared/auditorAware/PersistenceConfig.java
+++ src/main/java/com/walletmanagement/shared/auditorAware/PersistenceConfig.java
@@ -1,17 +1,23 @@
 package com.walletmanagement.shared.auditorAware;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.walletmanagement.entities.UserEntity;
+import com.walletmanagement.modules.user.UserRepository;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
 public class PersistenceConfig {
+
+  private final UserRepository userRepository;
 
   @Bean
   AuditorAware<UserEntity> auditorProvider() {
-    return new AuditorAwareImpl();
+    return new AuditorAwareImpl(this.userRepository);
   }
 
 }
