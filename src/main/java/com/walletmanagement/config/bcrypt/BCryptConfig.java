3dff583a7341916077da85eb466d22fd8f9f973f
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-01 19:59:59 -0300

    chore: move bcyrpt to a config class as a bean

---
+++ src/main/java/com/walletmanagement/config/bcrypt/BCryptConfig.java
@@ -0,0 +1,15 @@
+package com.walletmanagement.config.bcrypt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BCryptConfig {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
+
+}
